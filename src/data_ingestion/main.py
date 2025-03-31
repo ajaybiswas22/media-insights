69475f27d72b9da39067a2dfea2a3c3e401ce8ed
Ajay Biswas <ajaybiswas22@gmail.com>
2025-04-01 04:42:23 +0530

    Added dag (#10)

--- src/data_ingestion/main.py
+++ src/data_ingestion/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from utils.youtube_client import YoutubeClient
 from utils.vault_client import VaultClient
+from utils.minio_client import MinioClient
 import json
 import os
 from dotenv import load_dotenv
@@ -18,4 +19,8 @@ youtube = YoutubeClient(YOUTUBE_API_KEY)
 
 response = youtube.search('snippet','monster energy drink',1)
 
-print(json.dumps(response,indent=2))
+minio = MinioClient(minio_endpoint=minio_endpoint, minio_access_key=minio_access_key, minio_secret_key=minio_secret_key)
+minio.upload_json("youtube", "response.json", response, overwrite=True)
+response_json = minio.download_json("youtube", "response.json")
+
+print(response_json)
--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -1,3 +1,10 @@
-"""Yet to setup
-"""
-print('hello app')
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
