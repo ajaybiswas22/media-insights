b2e4782183848543beb74ef8bc70cbc5b98973ac
Ajay Biswas <ajaybiswas22@gmail.com>
2025-04-07 21:08:22 +0530

    Dev (#15)
    * Added dag
    
    * minor fix (#11)
    
    * updated youtube api (#12)
    
    * Feat/8/spark setup (#14)
    
    * Dev (#13)
    
    * Added dag
    
    * minor fix (#11)
    
    * updated youtube api (#12)
    
    * spark setup
    
    * fixed spark and minio

--- src/airflow/dags/youtube_search.py
+++ src/airflow/dags/youtube_search.py
@@ -4,20 +4,26 @@ from datetime import datetime
 
 default_args = {
     "owner": "airflow",
-    "start_date": datetime(2025, 3, 31),
+    "start_date": datetime(2025, 4, 2),
     "retries": 1,
 }
 
 with DAG(
     "youtube_search",
     default_args=default_args,
-    schedule_interval=None,  # Trigger manually or via API
-    catchup=False,
+    schedule_interval="@daily",  # Runs daily, change as needed
+    catchup=True,  # Allows DAG to run for missed execution dates
 ) as dag:
 
     run_youtube_api = BashOperator(
         task_id="youtube_ingest",
-        bash_command="docker exec data_ingestion python /app/youtube_ingest.py 2025-04-01"
+        bash_command="docker exec data_ingestion python /app/youtube_ingest.py {{ ds }}"
     )
 
-    run_youtube_api
+    run_bronze_search = BashOperator(
+        task_id="bronze_search",
+        bash_command="docker exec data_engineering python /app/01_search.py"
+    )
+
+    run_youtube_api >> run_bronze_search
+
