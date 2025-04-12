432454c1830822a025148387b453386c8cab7b68
Ajay Biswas <ajaybiswas22@gmail.com>
2025-04-13 00:35:06 +0530

    added delta support (#23)

--- README.md
+++ README.md
@@ -1,25 +1,22 @@
 # media-insights
 Provides insights on social media content
 
-# Running Locally
+## Running Locally
 
 1. If poetry is not installed
 ```
-brew install poetry
-```
-2. Create virtual environment
-```
-poetry config virtualenvs.create false
-python3 -m venv mi-env
-source mi-env/bin/activate
-```
-3. Install dependencies
-```
-poetry lock
-poetry install
+pipx install poetry
 ```
 
-# Running using Docker
+2. Install dependencies
+```
+poetry config virtualenvs.create true
+poetry lock
+poetry install
+source .venv/bin/activate
+```
+
+## Running using Docker
 
 ### Create self signed certificate for vault
 ```
@@ -48,8 +45,26 @@ docker exec -it spark-worker /opt/bitnami/spark/bin/spark-submit \
   --master spark://spark-master:7077 \
   --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:/opt/bitnami/spark/conf/log4j.properties" \
   --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:/opt/bitnami/spark/conf/log4j.properties" \
-  /opt/bitnami/spark/data_engineering/01_search.py 2025-04-03
+/opt/bitnami/spark/data_engineering/01_search.py 2025-04-02
 ```
+
+## Container List with ports
+
+1. airflow webserver                8081->8080
+2. bitnami/spark-worker             8083->8081
+3. bitnami/spark-master             7077->7077, 8082->8080
+4. bitnami/spark-history-server     18080->18080
+5. bitnami/minio                    9000-9001->9000-9001
+6. data_ingestion                   N/A
+7. flask_app                        8000->8000
+8. hashicorp/vault                  8200->8200
+9. postgres:13                      5432/tcp
+10. redis:latest                    6379/tcp
+11. airflow init
+12. airflow worker
+13. airflow scheduler
+14. airflow trigerrer
+
 
 ### Future diagrams
 ```mermaid
--- src/utils/spark_client.py
+++ src/utils/spark_client.py
@@ -3,13 +3,30 @@ from pyspark.sql import SparkSession
 class SparkSessionManager:
 
     @staticmethod
-    def get_spark_session(app_name: str, master: str = "spark://spark:7077", executor_memory: str = "1g", executor_cores: str = "2"):
+    def get_spark_session(app_name: str, master: str = "spark://spark:7077", executor_memory: str = "1g", executor_cores: str = "2", storage_client: object = None):
         
-        return SparkSession.builder \
-        .appName(app_name) \
-        .master(master) \
-        .config("spark.executor.memory", executor_memory) \
-        .config("spark.executor.cores", executor_cores) \
-        .config("spark.eventLog.enabled", "true") \
-        .config("spark.eventLog.dir", "file:/opt/bitnami/spark/spark-events") \
-        .getOrCreate()
+        if storage_client:
+            return SparkSession.builder \
+            .appName(app_name) \
+            .master(master) \
+            .config("spark.executor.memory", executor_memory) \
+            .config("spark.executor.cores", executor_cores) \
+            .config("spark.eventLog.enabled", "true") \
+            .config("spark.eventLog.dir", "file:/opt/bitnami/spark/spark-events") \
+            .config("spark.hadoop.fs.s3a.access.key", storage_client.access_key) \
+            .config("spark.hadoop.fs.s3a.secret.key", storage_client.secret_key) \
+            .config("spark.hadoop.fs.s3a.endpoint", storage_client.endpoint) \
+            .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+            .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
+            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+            .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+            .getOrCreate()
+        else:
+            return SparkSession.builder \
+            .appName(app_name) \
+            .master(master) \
+            .config("spark.executor.memory", executor_memory) \
+            .config("spark.executor.cores", executor_cores) \
+            .config("spark.eventLog.enabled", "true") \
+            .config("spark.eventLog.dir", "file:/opt/bitnami/spark/spark-events") \
+            .getOrCreate()
